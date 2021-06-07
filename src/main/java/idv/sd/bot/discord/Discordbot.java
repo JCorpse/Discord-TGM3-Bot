ea1984deb41c03436ef2f9761021ff9455c150fe
Juu <24427007+JCorpse@users.noreply.github.com>
2021-06-07 15:02:27 +0800

    make it better

---
+++ src/main/java/idv/sd/bot/discord/Discordbot.java
@@ -0,0 +1,49 @@
+package idv.sd.bot.discord;
+
+
+import discord4j.common.util.Snowflake;
+import discord4j.core.DiscordClientBuilder;
+import discord4j.core.GatewayDiscordClient;
+
+import discord4j.core.event.domain.Event;
+import discord4j.core.event.domain.message.MessageCreateEvent;
+import discord4j.core.object.entity.Guild;
+import discord4j.core.object.entity.channel.Channel;
+import discord4j.core.object.entity.channel.GuildChannel;
+import discord4j.discordjson.Id;
+import discord4j.discordjson.json.GuildData;
+import discord4j.rest.entity.RestChannel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Discordbot {
+    private final Logger logger = LoggerFactory.getLogger(Discordbot.class);
+    private final String TOKEN = "";
+    private final String ChannelId = "";
+    private GatewayDiscordClient Client;
+
+    public Discordbot() {
+        start();
+    }
+
+    public static void main(String[] args) {
+        Discordbot a = new Discordbot();
+        a.start();
+    }
+
+    private void start() {
+        try {
+            Client = DiscordClientBuilder.create(TOKEN)
+                    .build()
+                    .login()
+                    .block();
+//            Client.onDisconnect().block();
+        } catch (Exception e) {
+            logger.error("Discordbot start error", e);
+        }
+    }
+
+    public void sendMsg(String content) {
+        Client.getRestClient().getChannelById(Snowflake.of(Id.of(ChannelId))).createMessage(content).subscribe();
+    }
+}
---
+++ src/main/java/idv/sd/bot/twitch/Twitchbot.java
@@ -0,0 +1,106 @@
+package idv.sd.bot.twitch;
+
+import com.github.philippheuer.credentialmanager.domain.OAuth2Credential;
+import com.github.twitch4j.TwitchClient;
+import com.github.twitch4j.TwitchClientBuilder;
+import com.github.twitch4j.common.events.TwitchEvent;
+import com.github.twitch4j.helix.domain.UserList;
+import com.github.twitch4j.pubsub.events.*;
+import idv.sd.bot.discord.Discordbot;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.text.DecimalFormat;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.util.Arrays;
+import java.util.Locale;
+import java.util.function.Consumer;
+
+public class Twitchbot {
+    private final Logger logger = LoggerFactory.getLogger(Twitchbot.class);
+    private final String OAUTH = "";
+    private final String ChannelName = "";
+    private OAuth2Credential Credential = new OAuth2Credential("twitch", OAUTH);
+    private Discordbot Discordbot = new Discordbot();
+    private DateTimeFormatter Formatter;
+    private TwitchClient Client;
+    private String ChannelId;
+    private Integer LastLevel;
+    private Integer LastProgress;
+    private Integer LastGoal;
+//    private String HypeTrainID;
+
+    public static void main(String[] args) {
+        Twitchbot a = new Twitchbot();
+        a.start();
+    }
+
+    private void start() {
+        init();
+        getChannelId(ChannelName);
+        HypeTrainlistener();
+    }
+
+    private void init() {
+        Client = TwitchClientBuilder.builder()
+                .withEnablePubSub(true)
+                .withEnableHelix(true)
+                .withDefaultAuthToken(Credential)
+                .build();
+        Formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss")
+                .withLocale(Locale.TAIWAN)
+                .withZone(ZoneId.of("Asia/Taipei"));
+    }
+
+    public void getChannelId(String ChannelName) {
+        UserList resultList = Client.getHelix().getUsers(null, null, Arrays.asList(ChannelName)).execute();
+        resultList.getUsers().forEach(user -> {
+            ChannelId = user.getId();
+        });
+    }
+
+    public void HypeTrainlistener() {
+        Client.getPubSub().listenForHypeTrainEvents(Credential, ChannelId);
+//        Client.getEventManager().onEvent(HypeTrainStartEvent.class, (Event) -> {
+//            HypeTrainID = Event.getData().getId();
+//        });
+
+        Client.getEventManager().onEvent(HypeTrainProgressionEvent.class, (Event) -> {
+            System.out.println(Event);
+            LastLevel = Event.getData().getProgress().getLevel().getValue();
+            LastProgress = Event.getData().getProgress().getValue();
+            LastGoal = Event.getData().getProgress().getGoal();
+            String Percent = (new DecimalFormat("#.##").format(((float) LastProgress / LastGoal) * 100));
+            System.out.println(LastLevel + "-" + Percent + "%");
+            System.out.println(LastLevel + "-" + Percent + "%");
+
+        });
+        Client.getEventManager().onEvent(HypeTrainLevelUpEvent.class, (Event) -> {
+            System.out.println(Event);
+            LastLevel = Event.getData().getProgress().getLevel().getValue();
+            LastProgress = Event.getData().getProgress().getValue();
+            LastGoal = Event.getData().getProgress().getGoal();
+        });
+        Client.getEventManager().onEvent(HypeTrainEndEvent.class, (Event) -> {
+            System.out.println(Event);
+            Instant EndTime = Event.getData().getEndedAt();
+            String Percent = (new DecimalFormat("#.##").format((float) (LastProgress / LastGoal) * 100));
+            Discordbot.sendMsg(
+                    "==== 列車發車記錄(v0.3) ====\n" +
+                            "列車離站時間: " + Formatter.format(EndTime) + "\n" +
+                            "貼圖等級: " + LastLevel + "-" + Percent + "%\n" +
+                            "========================");
+        });
+//        Client.getEventManager().onEvent(HypeTrainConductorUpdateEvent.class, (Event) -> {
+//            System.out.println(Event);
+//            Discordbot.sendMsg(Event.getData().toString());
+//        });
+//        Client.getEventManager().onEvent(HypeTrainCooldownExpirationEvent.class, (Event) -> {
+//            System.out.println(Event);
+//            Discordbot.sendMsg(Event.getFiredAt().toString());
+//        });
+    }
+}
