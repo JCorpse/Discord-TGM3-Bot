c89e53e1b766186d6b5052297940522565d4bf94
JCorpse <24427007+JCorpse@users.noreply.github.com>
2022-03-23 16:49:29 +0800

    try createEmbed

--- src/main/java/idv/sd/api/gov/cwb/Cwd_Earthquake.java
+++ src/main/java/idv/sd/api/gov/cwb/Cwd_Earthquake.java
@@ -3,6 +3,7 @@ package idv.sd.api.gov.cwb;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import discord4j.core.spec.EmbedCreateSpec;
+import discord4j.rest.util.Color;
 import idv.sd.api.gov.constant.GovConstant;
 
 import java.net.URI;
@@ -10,6 +11,8 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.time.Duration;
+import java.time.Instant;
+import java.util.Random;
 
 public class Cwd_Earthquake {
     private  static final String API_KEY = GovConstant.CWD_KEY.getKey();
@@ -40,10 +43,17 @@ public class Cwd_Earthquake {
             ObjectMapper mapper = new ObjectMapper();
             JsonNode ResJSON = mapper.readTree(Res.body());
             if (!ResJSON.isEmpty()) {
+                Random random = new Random(255);
                 Embed = EmbedCreateSpec.builder()
+                        .color(Color.of(random.nextInt(),random.nextInt(),random.nextInt()))
                         .title(ResJSON.with("records").get("datasetDescription").asText()+"(v0.1)")
-                        .description(ResJSON.with("records").get("earthquake").get(0).get("reportContent").asText())
+                        .description(ResJSON.with("records").get("earthquake").get(0).get("reportContent").asText().replaceAll("\"[0-9]{2}/[0-9]{2}-[0-9]{2}:[0-9]{2}\"",""))
                         .image(ResJSON.with("records").get("earthquake").get(0).get("reportImageURI").asText())
+                        .url(ResJSON.with("records").get("earthquake").get(0).get("web").asText())
+                        .addField(ResJSON.with("records").get("earthquake").get(0).get("earthquakeInfo").get("magnitude").get("magnitudeType").asText(),ResJSON.with("records").get("earthquake").get(0).get("earthquakeInfo").get("magnitude").get("magnitudeValue").asText(),true)
+                        .addField("發生時間",ResJSON.with("records").get("earthquake").get(0).get("earthquakeInfo").get("originTime").asText(),true)
+                        .addField("發生位置",ResJSON.with("records").get("earthquake").get(0).get("earthquakeInfo").get("epiCenter").get("location").asText(),true)
+                        .timestamp(Instant.now())
                         .build();
 //                Report[0] = "==== " +  + " ====\n";
 //                Report[0] += ResJSON.with("records").get("earthquake").get(0).get("reportContent").asText() + "\n";
